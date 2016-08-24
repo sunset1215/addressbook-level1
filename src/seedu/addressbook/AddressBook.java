45d146f774d8691704a9f325601820cf4d6b36d8
Vivian <Vivian@Vivian-PC>
2016-08-24 18:29:57 +0800

    Edit method splitCommandWordAndArgs(String rawUserInput)
    Wrapped the condition in brackets

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -369,7 +369,7 @@ public class AddressBook {
      */
     private static String[] splitCommandWordAndArgs(String rawUserInput) {
         final String[] split =  rawUserInput.trim().split("\\s+", 2);
-        return split.length == 2 ? split : new String[] { split[0] , "" }; // else case: no parameters
+        return (split.length == 2) ? split : new String[] { split[0] , "" }; // else case: no parameters
     }
 
     /**
